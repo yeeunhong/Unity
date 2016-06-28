86624bd5877d543a9dcae94a9cf568ada1bcf912
purehero <purehero@inka.co.kr>
2016-06-28 19:14:44 +0900

    추가로  소스코드 등록

---
+++ MyStudy02/Assets/RotateScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour {
+
+    public float speed;
+    
+	// Use this for initialization
+	void Start () {
+    }
+	 
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    void OnGUI()
+    {
+    }
+}
